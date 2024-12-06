0cd181756d26ae21ec6f34f51e31765fa7de497f
Mylena Marques Ribeiro <ribeiro.mylena1@gmail.com>
2024-12-06 13:39:17 -0300

    Tornando a propriedade 'level' privada e acessando-a com Get

--- src/modules/personagem.js
+++ src/modules/personagem.js
@@ -2,17 +2,21 @@ export class Personagem {
     nome;
     vida = 100;
     mana = 100;
-    level;
+    #level;
     tipo;
     descricao;
 
-    constructor(nome, level){
+    constructor(nome){
         this.nome = nome;
-        this.level = level;
+        this.#level = 1;
+    }
+
+    get level() {
+        return this.#level;
     }
 
     obterInsignia() {
-        if(this.level >= 5) {
+        if(this.#level >= 5) {
             return `Implacável ${this.constructor.tipo}` 
         } else {
             return `${this.constructor.tipo} iniciante`
